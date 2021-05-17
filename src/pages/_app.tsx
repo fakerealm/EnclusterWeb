aa4ce076649f20cf3c3df9387f335f9399f04555
samrath2007 <samrath1324@gmail.com>
2021-05-17 17:48:26 +0530

    Made footer global

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { AppProps } from 'next/app';
 
+import { Footer } from '../components/Footer';
 import '../styles/main.css';
 
 // eslint-disable-next-line react/jsx-props-no-spreading
-const MyApp = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />;
-
+const MyApp = ({ Component, pageProps }: AppProps) => (
+    <>
+        <Component {...pageProps} />
+        <Footer />
+    </>
+)
 export default MyApp;
